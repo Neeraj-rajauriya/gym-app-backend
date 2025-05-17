f01c45926b8c994e0f7f4c102e37f37941a52f60
NeerajRajauriya02 <neeraj@trupulse.ai>
2025-05-18 00:35:38 +0530

    Adding some more API's to payment feature

--- src/controllers/paymentContoller.js
+++ src/controllers/paymentContoller.js
@@ -1,30 +1,98 @@
-import {  payment } from "../models/payment.model.js";
+import { payment } from "../models/payment.model.js";
 import { userMembership } from "../models/usermembership.model.js";
-import {user} from "../models/user.model.js";
+import { user } from "../models/user.model.js";
+import { MembershipPlan } from "../models/membership.model.js";
 
-export const createPayment=async(req,res)=>{
-    try{
-        const userId=req.user._id;
-        const {userMembershipId,amount}=req.body;
-        const membership=await userMembership.findById(userMembershipId).populate("membershipPlanId");
-        if(!membership){
-            return res.status(404).json({Success:false,message:"Membership Not Found!"});
-        }
-
-        const planAmount=membership.membershipPlanId.price;
-        if(amount !== planAmount){
-            return res.status(400).json({Success:true,message:"Invalid amount"});
-        }
-
-        const payment=await payment.create({
-            userId,
-            userMembershipId,
-            amount,
-            status:"Pending"
-        })
-        res.status(201).json({Success:true,message:"Payment Initiated"},payment);
-    }catch(err){
-        console.log("Error",err.message);
-        res.status(500).json({Success:false,message:"Internal Server Error"});
+export const createPayment = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { userMembershipId, amount } = req.body;
+    const membership = await userMembership
+      .findById(userMembershipId)
+      .populate("membershipPlanId");
+    if (!membership) {
+      return res
+        .status(404)
+        .json({ Success: false, message: "Membership Not Found!" });
     }
-}
+
+    const planAmount = membership.membershipPlanId.price;
+    if (amount !== planAmount) {
+      return res.status(400).json({ Success: true, message: "Invalid amount" });
+    }
+    const newPayment = await payment.create({
+      userId,
+      userMembershipId,
+      amount,
+      status: "Pending",
+    });
+    console.log("newPayment is", newPayment);
+    res
+      .status(201)
+      .json({ Success: true, message: "Payment Initiated", newPayment });
+  } catch (err) {
+    console.log("Error", err.message);
+    res.status(500).json({ Success: false, message: "Internal Server Error" });
+  }
+};
+
+export const updatePaymentStatus = async (req, res) => {
+  try {
+    const { status } = req.body;
+    const { id } = req.params;
+
+    const existingPayment = await payment.findById(id);
+    if (!existingPayment) {
+      return res
+        .status(404)
+        .json({ Sucess: false, message: "Payment Not Found!" });
+    }
+    existingPayment.status = status;
+    await existingPayment.save();
+    if (existingPayment.status === "Success") {
+      const membership = await userMembership
+        .findById(existingPayment.userMembershipId)
+        .populate("membershipPlanId");
+      const startDate = new Date();
+      const endDate = new Date();
+      endDate.setMonth(
+        startDate.getMonth() + membership.membershipPlanId.duration
+      );
+      membership.paymentStatus = "Success";
+      membership.startDate = startDate;
+      membership.endDate = endDate;
+
+      await membership.save();
+    }
+    res
+      .status(200)
+      .json({
+        Success: true,
+        message: "Payment Updated Successfully",
+        existingPayment,
+      });
+  } catch (err) {
+    console.log("Error", err.message);
+    res.status(500).json({ Success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getAllpayment = async (req, res) => {
+  try {
+    console.log("function calling getAllPayemnt");
+    const payments = await payment
+      .find()
+      .populate("userId", "name email")
+      .populate({
+        path: "userMembershipId",
+        populate: { path: "membershipPlanId" },
+      });
+    console.log("Payment", payments);
+    res
+      .status(200)
+      .json({ Sucess: true, message: "All Payments Fetched", payments });
+  } catch (err) {
+    console.log("Error", err.message);
+    res.status(500).json({ Success: false, message: "Internal Server Error" });
+  }
+};
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createPayment } from "../controllers/paymentContoller.js";
+import { createPayment, updatePaymentStatus, getAllpayment } from "../controllers/paymentContoller.js";
 import {authMiddleware} from "../middleware/auth.middleware.js";
 import { roleMiddleware } from "../middleware/role.middleware.js";
 const paymentRouter=express.Router();
 
 paymentRouter.post('/',authMiddleware,createPayment);
+paymentRouter.put('/:id',authMiddleware,updatePaymentStatus);
+paymentRouter.get('/',authMiddleware,roleMiddleware("admin"),getAllpayment);
+
 
 
 export default paymentRouter;
